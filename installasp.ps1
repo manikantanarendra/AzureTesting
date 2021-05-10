1cc31112ca838de789f791391b7da8b8aca853b4
manikantanarendra <55499320+manikantanarendra@users.noreply.github.com>
2021-05-10 16:49:21 +0530

    Update installasp.ps1

--- installasp.ps1
+++ installasp.ps1
@@ -1,14 +1,11 @@
 # Install IIS
 Install-WindowsFeature Web-Server,Web-Asp-Net45,NET-Framework-Features
-
 # Install the .NET Core SDK
 Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848827 -outfile $env:temp\dotnet-dev-win-x64.1.0.6.exe
 Start-Process $env:temp\dotnet-dev-win-x64.1.0.6.exe -ArgumentList '/quiet' -Wait
-
 # Install the .NET Core Windows Server Hosting bundle
 Invoke-WebRequest https://dotnet.microsoft.com/permalink/dotnetcore-current-windows-runtime-bundle-installer -outfile $env:temp\DotNetCore.WindowsHosting.exe
 Start-Process $env:temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
-
 # Restart the web server so that system PATH updates take effect
 Stop-Service was -Force
 Start-Service w3svc
